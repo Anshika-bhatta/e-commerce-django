50ce6f2decf644b890a4635cf6d8386817d05d67
unknown <anshikabhatt83@gmail.com>
2025-07-12 12:16:19 +0545

    python

---
+++ class1.py
@@ -0,0 +1,36 @@
+# print("Hello World")
+
+# name = "Anshika"
+# age = 123123
+# print(f"My name is {name}, I am {age} years old.")
+
+# a = 232 
+# b = 39282 
+# print(a+b)
+
+# age = 23648
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+    
+# for i in range(5):
+#     print(f"Hi {i}")
+    
+# def greet(name):
+#     return f"Hello, {name}!"
+# print(greet ("Anshika"))
+
+# class Student:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+# S1 = Student("Anshika",1231)
+# print(S1.name)  
+
+import turtle
+t = turtle.Turtle()
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+turtle.done()    
