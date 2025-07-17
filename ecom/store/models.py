ee1a08ae0fe7e7bd20f5ab68628aef612dd1a50c
unknown <anshikabhatt83@gmail.com>
2025-07-18 00:02:02 +0545

    cart

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name =models.CharField(max_length=50)
@@ -44,6 +45,18 @@ class Order(models.Model):
 
 def __str__(self):
     return self.Product
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+    def total_price(self):
+        return self.quantity * self.product.price
 
 
 
