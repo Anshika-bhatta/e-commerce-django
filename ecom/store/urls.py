0711b06129a737304d23dc191df0a50f10392eba
unknown <anshikabhatt83@gmail.com>
2025-07-24 11:10:51 +0545

    payment-method

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update-cart/<int:cart_item_id>/', views.update_cart, name='update_cart'),
+    
     path('checkout/', views.checkout, name='checkout'),
     path('process-payment/', views.process_payment, name='process_payment'),
     path('payment/success/', views.payment_success, name='payment_success'),
